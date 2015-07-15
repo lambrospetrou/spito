1a5a7d747c528eb040b465f7dab1a553205ed842
Lambros Petrou <lambrospetrou@gmail.com>
2015-07-16 00:14:07 +0100

    removed old website files completely

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# start the spitty executable
-./spito 2> /home/lambros/public/spito/log/stderr_spito.log 1> /home/lambros/public/spito/log/stdout_spito.log &
-
-# log its PID for easy temrination
-echo $! > RUNNING_PID
-
--- stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -e "RUNNING_PID" ] 
-then
-    cat RUNNING_PID | xargs kill -9 
-    rm -f RUNNING_PID
-fi
-
